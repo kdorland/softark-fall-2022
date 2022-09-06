87668ba887f1efb1ef305e36b55afab187b961e3
Kristian Dorland <kristian@dorland.dk>
2022-09-06 13:18:54 +0200

    Lidt til modul 3 og modul 4

---
+++ modul03/opgaver/opgave3/Program.cs
@@ -0,0 +1,13 @@
+﻿var CreateWordFilterFn = (string[] words) =>
+{
+    return (string filterBadWords) =>
+    {
+        return String.Join(" ", filterBadWords.Split(" ").Except(words));
+    };
+};
+
+
+var badWords = new string[] { "tis", "pis", "lort" };
+var FilterBadWords = CreateWordFilterFn(badWords);
+Console.WriteLine(FilterBadWords("Sikke en gang lort"));
+// Udskriver: "Sikke en gang kage"
