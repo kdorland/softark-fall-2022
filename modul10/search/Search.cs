b63a7fc75d61e32ced855781f2680c235275eb66
Kristian Dorland <kristian@dorland.dk>
2022-10-25 08:27:23 +0200

    Skabelon til modul 10

---
+++ modul10/search/Search.cs
@@ -0,0 +1,53 @@
+﻿namespace SearchMethods
+{
+    public class Search
+    {
+        /// <summary>
+        /// Finder tallet i arrayet med en lineær søgning.
+        /// </summary>
+        /// <param name="array">Det array der søges i.</param>
+        /// <param name="tal">Det tal der skal findes.</param>
+        /// <returns></returns>
+        public static int FindNumberLinear(int[] array, int tal) {
+            // TODO: Implement!
+            return -1;
+        }
+        /// <summary>
+        /// Finder tallet i arrayet med en binær søgning.
+        /// </summary>
+        /// <param name="array">Det array der søges i.</param>
+        /// <param name="tal">Det tal der skal findes.</param>
+        /// <returns></returns>
+        public static int FindNumberBinary(int[] array, int tal) {
+            // TODO: Implement!
+            return -1;
+        }
+
+        private static int[] sortedArray { get; set; } =
+            new int[] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+        private static int next = 0;
+
+        /// <summary>
+        /// Indsætter et helt array. Arrayet skal være sorteret på forhånd.
+        /// </summary>
+        /// <param name="sortedArray">Array der skal indsættes.</param>
+        /// <param name="next">Den næste ledige plads i arrayet.</param>
+        public static void InitSortedArray(int[] sortedArray, int next)
+        {
+            Search.sortedArray = sortedArray;
+            Search.next = next;
+        }
+
+        /// <summary>
+        /// Indsætter et tal i et sorteret array. En kopi af arrayet returneres.
+        /// Array er fortsat sorteret efter det nye tal er indsat.
+        /// </summary>
+        /// <param name="tal">Tallet der skal indsættes</param>
+        /// <returns>En kopi af det sorterede array med det nye tal i.</returns>
+        public static int[] InsertSorted(int tal) {
+            // TODO: Implement!
+            
+            return sortedArray;
+        }
+    }
+}
