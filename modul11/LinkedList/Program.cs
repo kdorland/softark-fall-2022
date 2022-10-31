cc732ba1dab04f9077dcbcf5938cdaae79f80d17
Kristian Dorland <kristian@dorland.dk>
2022-10-31 10:34:25 +0100

    Eksempler til modul 11

---
+++ modul11/LinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using LinkedList;
+
+User kristian = new User("Kristian", 1);
+User mads = new User("Mads", 2);
+User torill = new User("Torill", 3);
+User kell = new User("Kell", 4);
+User henrik = new User("Henrik", 5);
+User klaus = new User("Klaus", 6);
+
+UserLinkedList list = new UserLinkedList();
+list.AddFirst(kristian);
+list.AddFirst(mads);
+list.AddFirst(torill);
+list.AddFirst(henrik);
+list.AddFirst(klaus);
+
+Console.WriteLine(list.CountUsers());
+Console.WriteLine(list);
+
+list.RemoveUser(mads);
+list.RemoveFirst();
+
+Console.WriteLine(list.CountUsers());
+Console.WriteLine(list);
---
+++ modul11/LinkedList/UserLinkedList.cs
@@ -0,0 +1,87 @@
+﻿namespace LinkedList
+{
+    class Node
+    {
+        public Node(User data, Node next)
+        {
+            this.Data = data;
+            this.Next = next;
+        }
+        public User Data;
+        public Node Next;
+    }
+
+    class UserLinkedList
+    {
+        private Node first = null!;
+
+        public void AddFirst(User user)
+        {
+            Node node = new Node(user, first);
+            first = node;
+        }
+
+        public User RemoveFirst()
+        {
+            // TODO: Implement!
+            return null!;
+        }
+
+        public void RemoveUser(User user)
+        {
+            Node node = first;
+            Node previous = null!;
+            bool found = false;
+
+            while (!found && node != null)
+            {
+                if (node.Data.Name == user.Name)
+                {
+                    found = true;
+                    if (node == first)
+                    {
+                        RemoveFirst();
+                    }
+                    else
+                    {
+                        previous.Next = node.Next;
+                    }
+                }
+                else
+                {
+                    previous = node;
+                    node = node.Next;
+                }
+            }
+        }
+
+        public User GetFirst()
+        {
+            return first.Data;
+        }
+
+        public User GetLast()
+        {
+            // TODO: Implement
+            return null!;
+        }
+
+        public int CountUsers()
+        {
+            // TODO: Implement
+            return -1;
+        }
+
+        public override String ToString()
+        {
+            Node node = first;
+            String result = "";
+            while (node != null)
+            {
+                result += node.Data.Name + ", ";
+                node = node.Next;
+            }
+            return result.Trim();
+        }
+    }
+}
