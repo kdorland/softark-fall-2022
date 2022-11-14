79acc126ee4aaa6b55228fbc673ba9cbb5294657
Kristian Dorland <kristian@dorland.dk>
2022-11-14 11:06:39 +0100

    Sortering

---
+++ modul12/Sortering/InsertionSort.cs
@@ -0,0 +1,11 @@
+namespace Sortering;
+
+public class InsertionSort
+{
+
+    public static void Sort(int[] array)
+    {
+        // TODO!
+        return;
+    }
+}
---
+++ modul12/Sortering/SelectionSort.cs
@@ -0,0 +1,17 @@
+namespace Sortering;
+
+public class SelectionSort
+{
+    private static void Swap(int[] array, int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void Sort(int[] array)
+    {
+        // TODO!
+        return;
+    }
+}
